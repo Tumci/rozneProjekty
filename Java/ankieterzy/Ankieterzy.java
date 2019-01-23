6fbb1977022c289423566c20c6eaa4d1a8ad66a9
kamil Janiak <kamil.janiak.qa@gmail.com>
2019-01-23 21:56:21 +0100

    added list of what we can win and what wealth. added information about ankieters what they want to do

--- Java/ankieterzy/Ankieterzy.java
+++ Java/ankieterzy/Ankieterzy.java
@@ -15,19 +15,35 @@ public class Ankieterzy {
     static Kolory [] tabKoloryD = {zielony, niebieski, bialy, zolty, czerwony}; //Lista(kolejność) pytań ankietera D
     
     public static void main(String[] args){
-        System.out.println("Ankieter A przeprowadzi ankietę w mastępujący sposób.");
+        //tablica z informacjami o dostepnych nagrodach i ich wartosci
+        String [] kolor = {"zolty", "zielony", "czerwony", "bialy", "niebieski"};
+        int x = 0; // do przejscia po tablicy kolor
+        System.out.println("Lista przedmiotow do wygrania u ankieterow:");
+        for(Kolory wygrana: tabKoloryAB){
+            System.out.print("Po wybraniu odpowiedzi "+ kolor[x] +" respondent wygrywa "+ wygrana.getPRZEDMIOT());
+            System.out.println(" o wartosci " + wygrana.getWARTOSC()+" zl");
+            x++;
+        }
+        System.out.println(""); 
+        
+        System.out.println("(Ankieter pragnie dac jak najwiecej przedmiotow danemu respondentowi)");
+        System.out.println("Ankieter A przeprowadzi ankiete w mastepujący sposob.");
         Ankieter.ankieterA();
         System.out.println("");
         
-        System.out.println("Ankieter B przeprowadzi ankietę w mastępujący sposób.");
+        System.out.println("(Ankieter pragnie dac jak najmniej przedmiotow danemu respondentowi)");
+        System.out.println("Ankieter B przeprowadzi ankiete w mastepujący sposob.");
         Ankieter.ankieterB();
         System.out.println("");
         
-        System.out.println("Ankieter C przeprowadzi ankietę w mastępujący sposób.");
+        System.out.print("(Ankieter pragnie dac jak najwiecej przedmiotow");
+        System.out.println(", ale konczy zadawac pytania jeeli wartosc nagrod przekroczy 2500 zl)");
+        System.out.println("Ankieter C przeprowadzi ankiete w mastepujacy sposob.");
         Ankieter.ankieterC();
         System.out.println("");
         
-        System.out.println("Ankieter D przeprowadzi ankietę w mastępujący sposób.");
+        System.out.println("(Ankieter pragnie dac tylko albo zielony samochod albo dwie inne najbardziej warosciowe nagrody");
+        System.out.println("Ankieter D przeprowadzi ankiete w mastępujący sposob.");
         Ankieter.ankieterD();
         System.out.println("");
     }
